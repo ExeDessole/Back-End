dd5c5e133efaab0458f41da0e8fbec3bb2a960b4
exede <dessolem@yahoo.com>
2023-04-20 23:30:55 -0300

    Primer entrega de proyecto

---
+++ expressAvanzado/src/ProductManager.js
@@ -0,0 +1,91 @@
+import fs from 'fs';
+
+export default class ProductManager{
+  
+  constructor (path){
+      this.path = path;
+  };
+
+  getProduct = async () =>{
+    try {      
+      if (fs.existsSync(this.path)){
+        const data = await fs.promises.readFile(this.path, 'utf-8')        
+        const dataJSON = JSON.parse(data);  
+        return dataJSON;
+      }else{
+        return [];
+      }  
+    } catch (error) {
+      console.log(error);
+    }      
+  };
+
+
+  addProduct = async (product)=>{
+    try {
+      const products = await this.getProduct();
+   
+      if (products.length === 0){
+        product.id = 1;
+      }else{
+        product.id = products[products.length - 1].id +1;
+      }
+
+      products.push(product);
+
+      await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'));
+
+      return product;
+
+    } catch (error) {
+      console.log(error);
+    } 
+
+  };
+  
+  getProductByld = async (id) =>{
+    try {
+      const products = await this.getProduct();
+      const checkProduct = products.findIndex(product => product.id === id);
+    if (checkProduct === -1){
+      console.log('El producto no existe')
+    }else{
+      const check = products[checkProduct]       
+        return check;   
+    }
+    } catch (error) {
+      console.log(error);
+    }
+
+  };
+    
+  // updateProduct = (id,newPrice) =>{
+
+  //   const products = this.getProduct();
+  //   const checkProduct = products.findIndex(product => product.id === id);
+
+  //   const changePrice= products[checkProduct].price;
+
+  //   if (changePrice === newPrice){
+  //     console.log('El precio es igual al ingresado');
+  //   }else{
+  //     products[checkProduct].price = newPrice;
+  //     return;
+  //   };
+
+  // };
+  
+  // deleteProduct = (id) =>{
+  //   const products = this.getProduct();
+  //   const checkProduct = products.findIndex(product => product.id === id);
+    
+  //   if (checkProduct === -1){
+  //     console.log('El producto no existe');
+  //   }else{
+  //     delete products[checkProduct];
+  //     console.log('El producto ha sido eliminado');
+  //   }
+
+  // };
+  
+};
---
+++ expressAvanzado/src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import ProductManager from './ProductManager.js';
+
+// Instancio el modulo express
+const app = express();
+
+// Instancio la clase con la ruta de parametro
+const dataBase = new ProductManager('./products.json');
+//Método para tráfico de datos por url.
+app.use(express.urlencoded({extended: true}));
+// Instancio la ruta y dentro de la call llamo al metodo getProduct de mi ProductManager
+// y lo envío al servidor
+app.get('/products', async (req, res) => {
+    const products = await dataBase.getProduct();
+    res.send({products});
+    console.log(products);
+    
+});
+
+app.get('/products/:id/', async (req, res) => {
+    const productId = Number(req.params.id);
+    const product = await dataBase.getProductByld(productId)
+    if (!product) return res.send({error: 'Producto no encontrado'})
+    res.send(product);
+    console.log(product);
+});
+
+
+app.listen(8080, () => console.log('Listening on port 8080'));
